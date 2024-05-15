f6c428e2e940ca0956a5f9f0601f2d9f767cf46d
Padam Damai <padamdamai888@gmail.com>
2024-05-15 23:37:18 +0545

    added create operation

--- base/views.py
+++ base/views.py
@@ -7,4 +7,22 @@ def home(request):
         'emp':employ,
     }
     return render(request,'base/home.html',context)
-    
+
+def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+
+        emp = Employees(
+            name= name,
+            email= email,
+            address = address,
+            phone = phone,
+
+        )
+        emp.save()
+        return redirect('home')
+
+    return render(request,'base/home.html')
